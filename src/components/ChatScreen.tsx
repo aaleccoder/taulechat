cf2610df10ff27159915bd0b71f78a5cd6e9f481
daeralysdev <alejandroperezacosta105+git@gmail.com>
2025-09-06 13:55:25 -0400

    chore(release): bump to v0.2.5

--- src/components/ChatScreen.tsx
+++ src/components/ChatScreen.tsx
@@ -36,21 +36,14 @@ export default function ChatScreen() {
     };
   }, [id]);
 
-  return (
-    <div className="md:max-w-[70vw] w-full mx-auto">
-      <div
-        className="flex flex-col bg-background overflow-hidden py-40"
 
-      >
+  return (
+    <div className="md:max-w-[70vw] w-full mx-auto relative h-screen flex flex-col">
+      <div className="flex flex-col bg-background pt-20 pb-32 flex-1 overflow-hidden">
         <ChatMessages />
       </div>
-      <div
-        className="absolute inset-x-0 w-full px-2 pb-[env(safe-area-inset-bottom)] mb-4 flex justify-center"
-        style={{
-          transition: 'bottom 0.3s ease-out'
-        }}
-      >
-        <div className="w-full md:max-w-[80vw] flex-shrink-0">
+      <div className="absolute inset-x-0 bottom-0 w-full px-2 pb-[env(safe-area-inset-bottom)] flex justify-center">
+        <div className="w-full md:max-w-[80vw]">
           {/* @ts-ignore */}
           <ChatInput id={id} />
         </div>
